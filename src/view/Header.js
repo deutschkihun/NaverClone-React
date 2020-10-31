41dfe35378ab1ff766eb7e8523dde0744810ce52
deutschkihun <deutschkihun@gmail.com>
2020-10-31 13:46:17 +0100

    naver clone

---
+++ src/view/Header.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import '../css/global.css'
+import '../css/header.css'
+import naver_logo from '../images/naver_logo.png'
+
+
+function Header() {
+    return (
+        <div>
+           <header>
+
+            <div class="links">
+                <a href="/" className="link_text">네이버를 시작페이지로</a>
+                <a href="/" className="link_text">쥬니어네이버</a>
+                <a href="/" className="link_text">해피빈</a>
+            </div>
+            <a href="/"><img src={naver_logo} className="img_logo" /></a>
+
+            <form>
+                <fieldset>
+                    <legend className="visually-hidden">검색</legend>
+                    <div className="search_box">
+                        <input type="text" maxlength="225" tabindex="1"/>
+                        <button type="submit" tabindex="2">검색</button>
+                    </div>
+                </fieldset>
+            </form>
+
+            <nav>
+                <div className="nav_items">
+                    <ul>
+                        <li><a href="/">메일</a></li>
+                        <li><a href="/">카페</a></li>
+                        <li><a href="/">블로그</a></li>
+                        <li><a href="/">지식iN</a></li>
+                        <li><a href="/">쇼핑</a></li>
+                        <li><a href="/">Pay</a></li>
+                        <li><a href="/">TV</a></li>
+                        <li><a href="/">사전</a></li>
+                        <li><a href="/">뉴스</a></li>
+                        <li><a href="/">증권</a></li>
+                        <li><a href="/">부동산</a></li>
+                        <li><a href="/">지도</a></li>
+                        <li><a href="/">영화</a></li>
+                        <li><a href="/">뮤직</a></li>
+                        <li><a href="/">책</a></li>
+                        <li><a href="/">웹툰</a></li>
+                        <li><a href="/">더보기</a></li>
+                    </ul>
+                    <div className="keyword">
+                        <span className="color_naver">1</span>
+                        <span>Eoldan spark</span>
+                    </div>
+                </div>
+            </nav>
+
+            </header>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/view/Main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../css/global.css'
+import '../css/main.css'
+
+function Main() {
+    return (
+        <div>
+            <main>
+                <div class="main-container">
+                    <div class="temp-box box-one">1</div>
+                    <div class="temp-box box-two">2</div>
+                    <div class="temp-box box-three-1">3-1</div>
+                    <div class="temp-box box-three-2">3-2</div>
+                    <div class="temp-box box-four-1">4-1</div>
+                    <div class="temp-box box-four-2">4-2</div>
+                    <div class="temp-box box-five">5</div>
+                    <div class="temp-box box-six">6</div>
+                    <div class="temp-box box-seven">7</div>
+                    <div class="temp-box box-eight">8</div>
+                </div>
+            </main>
+
+        </div>
+    )
+}
+
+export default Main
